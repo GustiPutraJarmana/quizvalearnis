25e3c0e2ba4277a6b4f098418c083ce2b9ff52f1
Gusti Putra <gustiputra@Gustis-MacBook-Pro.local>
2023-02-03 23:59:25 +1100

    ls

--- src/routes/pages/HomeQuizPage.tsx
+++ src/routes/pages/HomeQuizPage.tsx
@@ -6,12 +6,12 @@ const HomeQuizPage: FC = (): ReactElement => {
   return (
     <AuthPage>
       <div className="relative flex flex-col overflow-hidden">
-        <div className="flex items-center justify-center h-screen flex-col z-30 text-black bg-slate-300 bg-opacity-70">
-          <div className="flex p-10 text-2xl md:text-5xl text-center items-center justify-center">
+        <div className="flex items-center justify-center h-screen flex-col z-30 bg-slate-300 bg-opacity-70">
+          <div className="flex p-10 text-2xl md:text-5xl text-center items-center justify-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-900">
             WELCOME TO VALEARNIS QUIZ
           </div>
-          <Link to="/quiz">
-            <button className="px-10 sm:px-[150px] py-2 sm:py-3 text-xl md:text-3xl bg-red-300 text-white hover:bg-red-400 hover:text-red-200 hover:shadow-lg">
+          <Link to="/quiz" reloadDocument>
+            <button className="px-10 sm:px-[150px] py-2 sm:py-3 text-xl md:text-3xl bg-red-300 text-white hover:bg-red-400 hover:text-red-200 hover:shadow-lg rounded-3xl">
               GET STARTED
             </button>
           </Link>
